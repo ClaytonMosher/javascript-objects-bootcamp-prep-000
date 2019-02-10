c7243caa3faf662c6828659107b2dab136b291d4
clayton j mosher <mosherclayton@gmail.com>
2019-02-10 04:46:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object ({keys: '0'});
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
-  return playist
+  return playlist
 }
 function removeFromPlaylist(playlist,artistName){
   delete playlist[artistName]
