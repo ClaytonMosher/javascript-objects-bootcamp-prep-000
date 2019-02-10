66fbc1cc441f1267c96e1beb87d8b23339197ce3
clayton j mosher <mosherclayton@gmail.com>
2019-02-10 04:44:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playist
 }
 function removeFromPlaylist(playlist,artistName){
-  delete playlist[artistName]l
+  delete playlist[artistName]
   return playlist
 }
