2f7a40fe2f703a64607985ba1ab4260015153780
clayton j mosher <mosherclayton@gmail.com>
2019-02-10 04:31:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,13 +1,2 @@
-var meals = {
-  breakfast: "oatmeal"
-};
-var meals = new 
-Object({ breakfast: 'oatmeal'})
-var meals = {
-breakfast: 'eggs',
-breakfast: 'bacon'}
-meals['breakfast']
-
-meals.snack = 'yogurt' //adding to an Object
-meals['second breakfast'] = 'bagel'
+var = playlist 
 
