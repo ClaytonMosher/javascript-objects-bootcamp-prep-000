07d8ba596cded3184e6d637b027bb7aece6f075b
clayton j mosher <mosherclayton@gmail.com>
2019-02-10 04:24:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,2 +1,13 @@
-var meals = {};
-Object();
+var meals = {
+  breakfast: "oatmeal"
+};
+var meals = new 
+Object({ breakfast: 'oatmeal'})
+var meals = {
+breakfast: 'eggs',
+breakfast: 'bacon'}
+meals['breakfast']
+
+meals.snack = 'yogurt' //adding to an Object
+meals['second breakfast'] = 'bagel'
+
